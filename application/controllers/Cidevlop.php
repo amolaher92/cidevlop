209410221f5e17ce9454e90df91cf62e7b89d040
Amol_Aher <amolaher92@gmail.com>
2021-09-25 12:30:08 +0530

    Added all css

--- application/controllers/Cidevlop.php
+++ application/controllers/Cidevlop.php
@@ -1,11 +1,19 @@
 <?php
-if(!class_exists('Cidevlop'))
-{
+defined('BASEPATH') or exit('No direct script access allowed');
+
+if (!class_exists('Cidevlop')) {
 	class Cidevlop extends CI_Controller
 	{
 		public function __construct()
 		{
 			parent::__construct();
+		}
+
+		public function index()
+		{
+			$this->load->view('header/header');
+			$this->load->view('home/home');
+			$this->load->view('footer/footer');
 		}
 	}
 }
---
+++ application/views/header/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--Head section-->
+<head>
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+	<meta name="author" content="Amol Aherrao">
+	<meta name="description" content="This is CI web development">
+	<meta name="keywords" content="CI web development">
+
+	<!-- Site title -->
+	<title>CI Web Development</title>
+
+	<!-- Bootstrap v4.6 -->
+	<link rel="stylesheet" href="<?= base_url('assets/bootstrap/dist/css/bootstrap.min.css');?>">
+
+	<!-- FontAwesome -->
+	<link rel="stylesheet" href="<?= base_url('assets/fontawesome-free/css/all.min.css');?>">
+
+	<!-- Ionics Icons -->
+	<link rel="stylesheet" href="<?= base_url('assets/ionic-icon/css/ionicons.min.css');?>">
+
+	<!-- Themify Icons -->
+	<link rel="stylesheet" href="<?= base_url('assets/themify-icons/themify-icons.css');?>">
+
+	<!-- Data Tables -->
+	<link rel="stylesheet" href="<?= base_url('assets/DataTables/css/dataTables.dataTables.min.css');?>">
+
+	<!-- Bootstrap Tree view -->
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-treeview.min.css');?>">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="<?= base_url('assets/OwlCarousel/dist/assets/owl.carousel.min.css');?>">
+	<link rel="stylesheet" href="<?= base_url('assets/OwlCarousel/dist/assets/owl.theme.default.min.css');?>">
+
+	<!-- Yet Element -->
+	<link rel="stylesheet" href="<?= base_url('assets/css/yet-element.css');?>">
+
+	<!-- Progressive loader -->
+	<link rel="stylesheet" href="<?= base_url('assets/pace-master/themes/blue/pace-theme-center-atom.css');?>">
+
+	<!-- catchaAPI google -->
+	<script defer src='https://www.google.com/recaptcha/api.js'></script>
+
+
