830cb01681e343f78b9b445037475c95b4cf554d
Amol_Aher <amolaher92@gmail.com>
2021-09-27 10:22:16 +0530

    Changes made in ignore file and add data to git

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -21,10 +21,10 @@
 
 	<!-- Ionics Icons -->
 	<link rel="stylesheet" href="<?= base_url('assets/lib/css/ionicons.min.css'); ?>">
-	<link rel="stylesheet" href="<?= base_url('assets/lib/css/ie7.css')?>">
+	<link rel="stylesheet" href="<?= base_url('assets/lib/css/ie7.css') ?>">
 
 	<!-- jQuery UI	-->
-	<link rel="stylesheet" href="<?= base_url('assets/lib/css/jquery-ui.min.css')?>">
+	<link rel="stylesheet" href="<?= base_url('assets/lib/css/jquery-ui.min.css') ?>">
 
 	<!-- Themify Icons -->
 	<link rel="stylesheet" href="<?= base_url('assets/lib/css/themify-icons.css'); ?>">
@@ -56,13 +56,53 @@
 <!--Start header-->
 <header>
 	<!--Start Navigations-->
-	<nav>
-		<ul>
-			<li><a href="#"></a></li>
-			<li><a href="#"></a></li>
-			<li><a href="#"></a></li>
-			<li><a href="#"></a></li>
-		</ul>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand"
+		   href="#">Navbar</a>
+		<button class="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavDropdown"
+				aria-controls="navbarNavDropdown"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse"
+			 id="navbarNavDropdown">
+			<ul class="navbar-nav">
+				<li class="nav-item active">
+					<a class="nav-link"
+					   href="<?= base_url('home') ?>">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link"
+					   href="#">Features</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link"
+					   href="#">Pricing</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle"
+					   href="#"
+					   id="navbarDropdownMenuLink"
+					   role="button"
+					   data-toggle="dropdown"
+					   aria-haspopup="true"
+					   aria-expanded="false">Dropdown link</a>
+					<div class="dropdown-menu"
+						 aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item"
+						   href="#">Action</a>
+						<a class="dropdown-item"
+						   href="#">Another action</a>
+						<a class="dropdown-item"
+						   href="#">Something else here</a>
+					</div>
+				</li>
+			</ul>
+		</div>
 	</nav>
 </header>
 <!--Start Main-->
